27317fbcaf7b47c52f43e0d551f76f8e182e2e08
216k155 <32459326+216k155@users.noreply.github.com>
2018-02-25 07:10:28 +1100

    Allow node send new block to all peers

--- src/main.cpp
+++ src/main.cpp
@@ -2785,7 +2785,7 @@ bool ActivateBestChain(CValidationState& state, CBlock* pblock)
                 BOOST_FOREACH (CNode* pnode, vNodes)
 
                    // Allow node send new block to all peers
-                   // if (chainActive.Height() > (pnode->nStartingHeight != -1 ? pnode->nStartingHeight - 2000 : nBlockEstimate))
+                      if (chainActive.Height() > (pnode->nStartingHeight != -1 ? pnode->nStartingHeight - 2000 : nBlockEstimate))
                         pnode->PushInventory(CInv(MSG_BLOCK, hashNewTip));
             }
             // Notify external listeners about the new tip.
